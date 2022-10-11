99c5137132ac7ec04e6804ac54b192347024efae
oriastanjung <oriastan999@gmail.com>
2022-10-11 12:38:57 +0700

    adding dynamic check on status view form

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -39,6 +39,7 @@ function Detail() {
   const handleClickFormTTE = () => {
     console.log("all data ==", allData);
     console.log("targetData ==", targetData);
+    console.log("status verifikasi ==", targetData.status_verifikasi);
   };
 
   const handleChange = (e) => {
@@ -61,30 +62,45 @@ function Detail() {
             <ViewSuratCard label={"Surat Permohonan Orangtua"} />
             <ViewSuratCard label={"Surat Keterangan Pindah Sekolah"} />
             <ViewSuratCard label={"Surat Keterangan PLH Kepala Sekolah"} />
-            <div className="verifikasiPenandatangan">
-              <h4 className="ms-3">Pilih Penandatangan :</h4>
-              <Form.Select onChange={handleChange}>
-                <option value={"Ketua Sub Bagian"}>Ketua Sub Bagian </option>
-                <option value={"Sekretaris DISDIK"}>Sekretaris DISDIK </option>
-              </Form.Select>
-            </div>
-            <div className="verifikasiPenandatangan">
-              <h4 className="ms-3">Jenis Surat yang akan Dikirim :</h4>
-              <Form.Select onChange={handleChange}>
-                <option value={"Surat Pindah Sekolah"}>
-                  Surat Pindah Sekolah{" "}
-                </option>
-                <option value={"Surat Pindah Rayon"}>
-                  Surat Pindah Rayon{" "}
-                </option>
-              </Form.Select>
-            </div>
-            <div className="formLaporanAction d-flex justify-content-end align-items-end flex-column my-4 gap-3 ">
-              <div>
-                <ButtonFormView>Verifikasi</ButtonFormView>
-              </div>
-              <p>*Verifikasi berkas dilakukan oleh Staff</p>
-            </div>
+            {targetData.status_verifikasi === "BELUM" ||
+            targetData.status_verifikasi === "belum" ||
+            targetData.status_verifikasi === 0 ||
+            targetData.status_verifikasi === false ? (
+              <>
+                <div className="verifikasiPenandatangan">
+                  <h4 className="ms-3">Pilih Penandatangan :</h4>
+                  <Form.Select onChange={handleChange}>
+                    <option value={"Ketua Sub Bagian"}>
+                      Ketua Sub Bagian{" "}
+                    </option>
+                    <option value={"Sekretaris DISDIK"}>
+                      Sekretaris DISDIK{" "}
+                    </option>
+                  </Form.Select>
+                </div>
+                <div className="verifikasiPenandatangan">
+                  <h4 className="ms-3">Jenis Surat yang akan Dikirim :</h4>
+                  <Form.Select onChange={handleChange}>
+                    <option value={"Surat Pindah Sekolah"}>
+                      Surat Pindah Sekolah{" "}
+                    </option>
+                    <option value={"Surat Pindah Rayon"}>
+                      Surat Pindah Rayon{" "}
+                    </option>
+                  </Form.Select>
+                </div>
+                <div className="formLaporanAction d-flex justify-content-end align-items-end flex-column my-4 gap-3 ">
+                  <div>
+                    <ButtonFormView>Verifikasi</ButtonFormView>
+                  </div>
+                  <p>*Verifikasi berkas dilakukan oleh Staff</p>
+                </div>
+              </>
+            ) : (
+              <p className="d-flex justify-content-center align-items-center text-status-form mt-5 mb-0">
+                SUDAH TERVERIFIKASI
+              </p>
+            )}
           </div>
         </FormCard>
 
@@ -92,39 +108,53 @@ function Detail() {
           <div className="mx-4 mt-3 mb-4 formCardHead">
             <h3 className="pb-3">Form Tanda Tangan Elektronik {"(TTE)"}</h3>
           </div>
-          <div className="d-flex flex-row justify-content-between align-items-center mx-4 mt-3 mb-4 px-4 gap-5">
-            <div className=" flex-grow-1">
-              <InputFormWithLabel
-                label={"Nomor Induk Pegawai"}
-                type={"number"}
-                name={"nip"}
-                placeholder={"Masukkan NIP"}
-                value={formTTE.nip}
-                onChange={handleChangeFormTTE}
-              />
-            </div>
+          {targetData.status_ttd === "BELUM" ||
+          targetData.status_ttd === "belum" ||
+          targetData.status_ttd === false ||
+          targetData.status_ttd === 0 ? (
+            <>
+              <div className="d-flex flex-row justify-content-between align-items-center mx-4 mt-3 mb-4 px-4 gap-5">
+                <div className=" flex-grow-1">
+                  <InputFormWithLabel
+                    label={"Nomor Induk Pegawai"}
+                    type={"number"}
+                    name={"nip"}
+                    placeholder={"Masukkan NIP"}
+                    value={formTTE.nip}
+                    onChange={handleChangeFormTTE}
+                  />
+                </div>
 
-            <div>
-              <InputFormWithLabel
-                label={"Masukkan KEYPHRASE"}
-                type={"password"}
-                name={"keyphrase"}
-                placeholder={"Masukkan Keyphrase"}
-                value={formTTE.keyphrase}
-                onChange={handleChangeFormTTE}
-              />
-            </div>
-          </div>
-          <div className="mx-5 mt-3 mb-4">
-            <div className="formLaporanAction d-flex justify-content-end align-items-end flex-column my-4 gap-3 ">
-              <div>
-                <ButtonFormView onClick={handleClickFormTTE} isprimary={"true"}>
-                  Proses TTE
-                </ButtonFormView>
+                <div>
+                  <InputFormWithLabel
+                    label={"Masukkan KEYPHRASE"}
+                    type={"password"}
+                    name={"keyphrase"}
+                    placeholder={"Masukkan Keyphrase"}
+                    value={formTTE.keyphrase}
+                    onChange={handleChangeFormTTE}
+                  />
+                </div>
               </div>
-              <p>*Pastikan anda adalah orang yang harus menandatangani</p>
-            </div>
-          </div>
+              <div className="mx-5 mt-3 mb-4">
+                <div className="formLaporanAction d-flex justify-content-end align-items-end flex-column my-4 gap-3 ">
+                  <div>
+                    <ButtonFormView
+                      onClick={handleClickFormTTE}
+                      isprimary={"true"}
+                    >
+                      Proses TTE
+                    </ButtonFormView>
+                  </div>
+                  <p>*Pastikan anda adalah orang yang harus menandatangani</p>
+                </div>
+              </div>
+            </>
+          ) : (
+            <p className="d-flex justify-content-center align-items-center text-status-form mt-5 mb-0">
+              SUDAH DI TANDATANGANI
+            </p>
+          )}
         </FormCard>
 
         <FormCard>
@@ -140,12 +170,22 @@ function Detail() {
               label={"Status Tanda Tangan Elektronik (TTE)"}
               status={targetData.status_ttd}
             />
-            <div className="formLaporanAction d-flex justify-content-end align-items-end flex-column my-4 gap-3 ">
-              <div>
-                <ButtonFormView>Kirim</ButtonFormView>
+
+            {targetData.status_kirim === "BELUM" ||
+            targetData.status_kirim === "belum" ||
+            targetData.status_kirim === false ||
+            targetData.status_kirim === 0 ? (
+              <div className="formLaporanAction d-flex justify-content-end align-items-end flex-column my-4 gap-3 ">
+                <div>
+                  <ButtonFormView>Kirim</ButtonFormView>
+                </div>
+                <p>*Pengiriman surat dilakukan oleh Staff</p>
               </div>
-              <p>*Pengiriman surat dilakukan oleh Staff</p>
-            </div>
+            ) : (
+              <p className="d-flex justify-content-center align-items-center text-status-form mt-5 mb-0">
+                SUDAH DIKIRIM
+              </p>
+            )}
           </div>
         </FormCard>
       </main>
