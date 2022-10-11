e0b8b3757938fbfe4e819eb8297f91d95cc53da2
oriastanjung <oriastan999@gmail.com>
2022-10-11 14:02:40 +0700

    add print paper feature button

--- src/components/ViewSuratCard/ViewSuratCard.js
+++ src/components/ViewSuratCard/ViewSuratCard.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import styles from "./ViewSuratCard.module.css";
 import iconEye from "../../assets/icon-eye.png";
-
+import { useReactToPrint } from "react-to-print";
 function ViewSuratCard(props) {
   const [show, setShow] = useState(false);
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+
+  const printArea = useRef();
+
   const handleSetShow = () => {
     setShow(!show);
   };
@@ -14,6 +17,11 @@ function ViewSuratCard(props) {
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
+
+  const handlePrint = useReactToPrint({
+    content: () => printArea.current,
+    documentTitle: "emp-data",
+  });
   return (
     <>
       <div className={`${styles.card} mb-2`}>
@@ -26,16 +34,27 @@ function ViewSuratCard(props) {
       </div>
 
       {show && (
-        <div className="d-flex justify-content-center align-items-center border border-secondary">
-          {props.pdfFile ? (
-            <Document
-              file={props.pdfFile}
-              onLoadSuccess={onDocumentLoadSuccess}
-            >
-              <Page pageNumber={pageNumber} />
-            </Document>
-          ) : (
-            <p className={styles.noSurat}>Surat Tidak Dilampirkan</p>
+        <div>
+          <div
+            // id="printarea"
+            ref={printArea}
+            className="d-flex justify-content-center align-items-center border border-secondary"
+          >
+            {props.pdfFile ? (
+              <Document
+                file={props.pdfFile}
+                onLoadSuccess={onDocumentLoadSuccess}
+              >
+                <Page pageNumber={pageNumber} renderInteractiveForms={true} />
+              </Document>
+            ) : (
+              <p className={styles.noSurat}>Surat Tidak Dilampirkan</p>
+            )}
+          </div>
+          {props.pdfFile && (
+            <div className={`${styles.buttonCetak} my-3 me-4`}>
+              <button onClick={handlePrint}>Cetak</button>
+            </div>
           )}
         </div>
       )}
